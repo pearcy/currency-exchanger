134f22e606e07537067e2b9b0d7adf21d52116a5
Joseph Pearce <joseph.pearce05@gmail.com>
2020-07-03 12:21:59 -0700

    solved error with open API

--- src/currency.js
+++ src/currency.js
@@ -9,9 +9,9 @@ export class DollarCurrency {
   async getRates() {
     try {
       // let process = "";
-      let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
-      
-      
+      // let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+      let response = await fetch(`https://open.exchangerate-api.com/v6/latest`);
+
       let jsonResponse;
       if (response.ok && response.status == 200) {
         jsonResponse = await response.json();
