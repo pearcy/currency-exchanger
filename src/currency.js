1eb6d009683673a9dfb6530f0b45edd0b667f46a
Joseph Pearce <joseph.pearce05@gmail.com>
2020-07-03 08:49:11 -0700

    add UI Logic - form input

--- src/currency.js
+++ src/currency.js
@@ -11,7 +11,6 @@ export class DollarCurrency {
      
       let response = await fetch(`https://v6.exchangerate-api.com/v6/YOUR-API-KEY/v6${process.env.API_KEY}/latest/USD`);
       
-    
       let jsonResponse;
       if (response.ok && response.status == 200) {
         jsonResponse = await response.json();
--- src/main.js
+++ src/main.js
@@ -4,5 +4,21 @@ import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
-import { DollarCurrency } from "./../src/currency.js";
+// import { DollarCurrency } from "./../src/currency.js";
+
+
+$(document).ready(function() {
+  $("#currency-form").submit(function(event) {
+    event.preventDefault();
+
+    let amount = parseInt($("#amount-input").val());
+    let currency = $("#currency-input").val();
+      console.log(currency, amount);
+
+
+
+
+
+  });
+});
 
