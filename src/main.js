5b53d69b9f4e12da046b2b89d42ea59e5c4d081b
Joseph Pearce <joseph.pearce05@gmail.com>
2020-07-03 09:51:58 -0700

    add async call, output in UI

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
-// import { DollarCurrency } from "./../src/currency.js";
+import { DollarCurrency } from "./../src/currency.js";
 
 
 $(document).ready(function() {
@@ -13,10 +13,23 @@ $(document).ready(function() {
 
     let amount = parseInt($("#amount-input").val());
     let currency = $("#currency-input").val();
-      console.log(currency, amount);
+      // console.log(currency, amount);
 
+      (async () => {
+        let newDollar = new DollarCurrency(amount, currency);
+        let response = await newDollar.getRates();
+        
+  
+        getElements(response);
+      })();
 
+      function getElements(response) {
+        if (response) {
+          $('.showCurrency').text(`The Currency for ${currency} is  ${response.conversion_rates}`);
+        }
+      }
 
+  
   });
 });
 
