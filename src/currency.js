a920f82fef26fd6edbf9b7193d571a4353e09174
Joseph Pearce <joseph.pearce05@gmail.com>
2020-07-03 07:49:25 -0700

    add API code

--- src/currency.js
+++ src/currency.js
@@ -6,7 +6,23 @@ export class DollarCurrency {
     this.currency = currency;
   }
 
-  
+  async getRates() {
+    try {
+     
+      let response = await fetch(`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}`);
+    
+      let jsonResponse;
+      if (response.ok && response.status == 200) {
+        jsonResponse = await response.json();
+        
+      } else {
+        jsonResponse = false; 
+      } 
+      console.log(jsonResponse);
+    } catch (error) {
+        return false; 
+      }
+  }
 
 
 
